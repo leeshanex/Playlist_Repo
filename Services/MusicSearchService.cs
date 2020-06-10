b92a2a0d3ae08e1e1224da99b772896be7ffd288
leeshanex <leeshanex@outlook.com>
2020-06-10 11:28:50 -0700

    Created a Services folder in project + Added a MusicSearchService class within service folder and implemented a GetMusicSearched method

---
+++ Services/MusicSearchService.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Playlist_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Playlist_Project.Services
+{
+    public class MusicSearchService
+    {
+        public MusicSearchService()
+        {
+
+        }
+        public async Task<MusicSearch> GetMusicSearched()
+        {
+            string url = $"https://api.deezer.com/search?redirect_uri=http%253A%252F%252Fguardian.mashape.com%252Fcallback&q=odesza&index=25";
+        HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<MusicSearch>(json);
+            }
+            return null;
+        }
+    }
+}
