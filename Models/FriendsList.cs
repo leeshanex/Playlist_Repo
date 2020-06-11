005bf25390fb0b00caa4605387c24bd4254ddc9a
Ryan Smith <ryan_smith2@hotmail.com>
2020-06-11 14:06:03 -0500

    added friendslist controller

---
+++ Models/FriendsList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Playlist_Project.Models
+{
+    public class FriendsList
+    {
+        public int Id { get; set; }
+        public string friendName { get; set; }
+
+        [ForeignKey("friendId1")]
+        public int friendId { get; set; }
+        public NewUser friendId1 { get; set; }
+
+        [ForeignKey("friendId2")]
+
+        public int friendIdTwo { get; set; }
+        public NewUser friendId2 { get; set; }
+    }
+}
